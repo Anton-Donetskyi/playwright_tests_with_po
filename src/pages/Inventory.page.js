f3c55bd72e7f72c3cb91d895061982bf25b22c95
Anton Donetskyi <antondonetckij@gmail.com>
2024-07-09 23:36:41 +0200

    started using pageObjects, added utilities, added test data

--- src/pages/Inventory.page.js
+++ src/pages/Inventory.page.js
@@ -7,9 +7,22 @@ export class InventoryPage extends BaseSwagLabPage {
 
     get inventoryItems() { return this.page.locator('.inventory_item'); }
 
-    get inventoryItemsTitles() { return this.page.locator('[data-test="inventory-item-name"]').innerTexts() }
-
     get addItemToCartBtns() { return this.page.locator('[id^="add-to-cart"]'); }
+
+    get itemsSortingOptions() { return this.page.locator('[data-test="product-sort-container"]'); }
+
+    async selectSortingOption(value) {
+        await this.itemsSortingOptions.selectOption(value);
+    }
+
+    async getAllInventoryItemsTitle() { 
+        return this.page.locator('[data-test="inventory-item-name"]').allInnerTexts();
+    }
+
+    async getAllInventoryItemsPrice() {
+        const pricesWithCurrency = await this.page.locator('[data-test="inventory-item-price"]').allInnerTexts();
+        return pricesWithCurrency.map(price => parseFloat(price.slice(1)));
+    }
 
     async addItemToCartById(id) {
         await this.addItemToCartBtns.nth(id).click();
--- src/tests/test1.spec.js
+++ src/tests/test1.spec.js
@@ -1,129 +1,66 @@
-/**
- * [
-  'Sauce Labs Backpack',
-  'Sauce Labs Bike Light',
-  'Sauce Labs Bolt T-Shirt',
-  'Sauce Labs Fleece Jacket',
-  'Sauce Labs Onesie',
-  'Test.allTheThings() T-Shirt (Red)'
-]
- */
-
-/**
- * [
-    "$29.99",
-    "$9.99",
-    "$15.99",
-    "$49.99",
-    "$7.99",
-    "$15.99",
-  ]
- */
-
 const { expect } = require('@playwright/test');
 const { test } = require('../fixture');
 
-const credentials = { 
-    userName: 'standard_user', 
-    password: 'secret_sauce'
-};
+import { standartUser } from '../data/users';
+import { sortAscending_A_Z, sortDescending_Z_A, sortAscending, sortDescending } from '../utils/sortItems';
 
 const sortParameters = ['az', 'za', 'lohi', 'hilo'];
 
-// Function to sort in ascending order for numeric values
-const sortAscending = (pricesArray) => {
-    return pricesArray.map(price => parseFloat(price.slice(1))).sort((a, b) => a - b);
-    //pricesArray.sort((a, b) => a - b);
-};
-
-// Function to sort in descending order for numeric values
-const sortDescending = (pricesArray) => {
-    return pricesArray.map(price => parseFloat(price.slice(1))).sort((a, b) => b - a);
-};
-
-// Function to sort in ascending order for string values
-const sortAscending_A_Z = (array) => {
-    return [...array].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-};
-
-// Function to sort in descending order for string values
-const sortDescending_Z_A = (array) => {
-    return [...array].sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()));
-};
-
-
 test.describe('Task 1 - Perform and verify sorting on the Inventory page', () => {
 
-    test.beforeEach('Login as standart_user', async ({ page }) => {
-        await page.goto('');
-        await page.getByPlaceholder('Username').fill(credentials.userName);
-        await page.getByPlaceholder('Password').fill(credentials.password);
-        await page.locator('[data-test="login-button"]').click();
+    test.beforeEach('Login as standart_user', async ({ loginPage, inventoryPage }) => {
+        await loginPage.navigate();
+        await loginPage.performLogin(standartUser.userName, standartUser.password);
+        await expect(inventoryPage.headerTitle).toBeVisible();
     });
 
-    test('Sort products by Titles ASC', async ({ page }) => {
-
-        // Get all products titles
-        const allItemTitles = await page.locator('[data-test="inventory-item-name"]').allInnerTexts();
-
-        // Call function to sort products by Titles in ASC
-        const sortedByTitlesASC = sortAscending_A_Z(allItemTitles);
+    test('Sort products by Titles ASC', async ({ inventoryPage }) => {
 
         // Click on the opion "A to Z" to sort products on UI
-        await page.locator('[data-test="product-sort-container"]').selectOption('az');
+        await inventoryPage.selectSortingOption('az');
 
-        // Get all products titles after sorting on UI
-        const actualSort = await page.locator('[data-test="inventory-item-name"]').allInnerTexts();
+        // Get all products titles
+        const allItemsTitle = await inventoryPage.getAllInventoryItemsTitle();
 
-        expect(actualSort).toEqual(sortedByTitlesASC);
+        // Call function to sort products by Titles in ASC
+        const sortedByTitlesASC = sortAscending_A_Z(allItemsTitle);
+
+        expect(allItemsTitle).toEqual(sortedByTitlesASC);
     }); 
 
-    test('Sort products by Titles DESC', async ({ page }) => {
+    test('Sort products by Titles DESC', async ({ inventoryPage }) => {
+
+        // Click on the opion "Z to A" to sort products on UI
+        await inventoryPage.selectSortingOption('za');
 
         // Store all product titles
-        const allItemTitles = await page.locator('[data-test="inventory-item-name"]').allInnerTexts();
+        const allItemsTitle = await inventoryPage.getAllInventoryItemsTitle();
 
-        const sortedByTitlesDESC = sortDescending_Z_A(allItemTitles);
-        await page.locator('[data-test="product-sort-container"]').selectOption('za');
+        // Sort products by title DESC
+        const sortedByTitlesDESC = sortDescending_Z_A(allItemsTitle);
 
-        const actualSort = await page.locator('[data-test="inventory-item-name"]').allInnerTexts();
-        expect(actualSort).toEqual(sortedByTitlesDESC);
-
+        expect(allItemsTitle).toEqual(sortedByTitlesDESC);
     });
 
-    test('Sort products by Prices ASC', async ({ page }) => {
+    test('Sort products by Prices ASC', async ({ inventoryPage }) => {
 
-        const AllItemPrices = await page.locator('[data-test="inventory-item-price"]').allInnerTexts();
+        // Click on the opion "low to high" to sort products on UI
+        await inventoryPage.selectSortingOption('lohi');
+        
+        const AllItemsPrice = await inventoryPage.getAllInventoryItemsPrice();
+        const sortedByPricesLowToHigh = sortAscending(AllItemsPrice);
 
-        const sortedByPricesLowToHigh = sortAscending(AllItemPrices);
-
-        await page.locator('[data-test="product-sort-container"]').selectOption('lohi');
-
-        let actualSort = (await page
-            .locator('[data-test="inventory-item-price"]')
-            .allInnerTexts())
-            .map(price => parseFloat(price.slice(1)));
-
-        //actualSort.map(price => parseFloat(price.slice(1)));
-
-        expect(actualSort).toEqual(sortedByPricesLowToHigh);
-
+        expect(AllItemsPrice).toEqual(sortedByPricesLowToHigh);
     });
 
-    test('Sort products by Prices DESC', async ({ page }) => {
+    test('Sort products by Prices DESC', async ({ inventoryPage }) => {
 
-        const AllItemPrices = await page.locator('[data-test="inventory-item-price"]').allInnerTexts();
+        // Click on the opion "high to low" to sort products on UI
+        await inventoryPage.selectSortingOption('hilo');
 
-        const sortedByPricesHighToLow = sortDescending(AllItemPrices);
+        const AllItemsPrice = await inventoryPage.getAllInventoryItemsPrice();
+        const sortedByPricesHighToLow = sortDescending(AllItemsPrice);
 
-        await page.locator('[data-test="product-sort-container"]').selectOption('hilo');
-
-        let actualSort = (await page
-            .locator('[data-test="inventory-item-price"]')
-            .allInnerTexts())
-            .map(price => parseFloat(price.slice(1)));
-
-        expect(actualSort).toEqual(sortedByPricesHighToLow);
-
+        expect(AllItemsPrice).toEqual(sortedByPricesHighToLow);
     });
 });
